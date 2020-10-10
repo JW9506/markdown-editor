7e1f7d4310409f46a49ab6f302f51c02d7c196b4
JW9506 <cf.dota@gmail.com>
2020-10-10 07:32:57 -0400

    configure tailwind

--- src/react-src/src/App.tsx
+++ src/react-src/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function App() {
-  return <div className="App">foobar</div>
+  return <div className="App flex justify-center items-center">foobar</div>
 }
 
 export default App
